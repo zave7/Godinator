981cfadf451f002919414f994e418e3602b890b5
mirae <alfo8360@naver.com>
2019-07-08 09:55:54 +0900

    20190708 memberpreferenceDto 항목수 추가(6개)

--- src/main/java/com/kitri/godinator/model/MemberPreferDto.java
+++ src/main/java/com/kitri/godinator/model/MemberPreferDto.java
@@ -11,10 +11,12 @@ public class MemberPreferDto {
 	private int hEval3; 		// 고등항목3
 	private int hEval4;			// 고등항목4
 	private int hEval5; 		// 고등항목5
+	private int hEval6; 		// 고등항목6
 	private int uEval1; 		// 대학항목1
 	private int uEval2; 		// 대학항목2
 	private int uEval3; 		// 대학항목3
 	private int uEval4; 		// 대학항목4
 	private int uEval5; 		// 대학항목5
+	private int uEval6; 		// 대학항목6
 
 }
