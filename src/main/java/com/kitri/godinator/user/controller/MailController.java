720f60795f6edaec51ef5b1434ac0ca47246c8b2
mirae <alfo8360@naver.com>
2019-07-12 02:05:04 +0900

    20190712 이메일 인증을 위한 설정

---
+++ src/main/java/com/kitri/godinator/user/controller/MailController.java
@@ -0,0 +1,44 @@
+package com.kitri.godinator.user.controller;
+
+import java.util.Random;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.kitri.godinator.user.service.MailService;
+
+
+@Controller
+public class MailController {
+    
+	@Autowired
+	private MailService mailService;
+ 
+	
+   
+ 
+    // 회원가입 이메일 인증
+    @RequestMapping(value = "/sendMail/auth", method = RequestMethod.POST, produces = "application/json")
+    @ResponseBody
+    public boolean sendMailAuth(HttpSession session, @RequestParam String email) {
+        int ran = new Random().nextInt(100000) + 10000; // 10000 ~ 99999
+        String joinCode = String.valueOf(ran);
+        session.setAttribute("joinCode", joinCode);
+ 
+        String subject = "회원가입 인증 코드 발급 안내 입니다.";
+        StringBuilder sb = new StringBuilder();
+        sb.append("귀하의 인증 코드는 " + joinCode + " 입니다.");
+        return mailService.send(subject, sb.toString(), "아이디@gmail.com", email, null);
+    }
+ 
+   
+ 
+   
+    }
+
