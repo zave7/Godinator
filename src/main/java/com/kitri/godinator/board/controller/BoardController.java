bac265860c4ec5b24dfded8ee149aafe58fcaf14
gwangq <pgg961@gmail.com>
2019-07-12 16:06:53 +0900

    19.07.12 다중 파일 업로드

--- src/main/java/com/kitri/godinator/board/controller/BoardController.java
+++ src/main/java/com/kitri/godinator/board/controller/BoardController.java
@@ -33,21 +33,20 @@ import com.kitri.godinator.model.PageNavigation;
 @Controller
 @RequestMapping("/board")
 public class BoardController {
-	
+
 	@Autowired
 	private ServletContext servletContext;
-	
+
 	@Autowired
 	private BoardCommonService boardCommonService;
-	
+
 	@Autowired
 	private BoardService boardService;
-	
+
 	@RequestMapping(value = "/main", method = RequestMethod.GET)
 	public String main(@RequestParam Map<String, String> parameter, Model model) {
 		return "board/main";
 	}
-	
 
 	// 게시판 list에서 글 작성 버튼 누르면 글 작성 페이지로 이동
 	@RequestMapping(value = "/write", method = RequestMethod.GET)
@@ -63,7 +62,6 @@ public class BoardController {
 		String schoolType = parameter.get("schoolType");
 		String schoolName = parameter.get("schoolName");
 
-
 		String json = "";
 		if ("고등학교".equals(schoolType)) {
 			json = boardService.findHSchool(schoolName);
@@ -76,69 +74,70 @@ public class BoardController {
 
 //------------------------[글쓰기]-------------------------
 	@RequestMapping(value = "/write", method = RequestMethod.POST)
-	public String write(BbsDto bbsDto, @RequestParam Map<String, String> parameter, 
-			@RequestParam("file") MultipartFile multipartFile ,Model model,
-			HttpSession session) {
-		//System.out.println("write controller in : 	" +parameter);
+	public String write(BbsDto bbsDto, @RequestParam Map<String, String> parameter,
+			@RequestParam("file") List<MultipartFile> multipartFile, Model model, HttpSession session) {
+		// System.out.println("write controller in : " +parameter);
 		MemberDto memberDto = (MemberDto) session.getAttribute("userInfo");
 		String path = "";
-		
+		System.out.println(multipartFile.toString());
+
 		if (memberDto != null) {
-			
+
 			int boardNo = boardCommonService.getNextBoardNo();
 			bbsDto.setBoardNo(boardNo);
 			bbsDto.setbUserId(memberDto.getUserId());
 			bbsDto.setUserName(memberDto.getUserName());
-			
-				if(multipartFile != null && !multipartFile.isEmpty()) {
-					String originalName = multipartFile.getOriginalFilename();
+
+			if (multipartFile != null && !multipartFile.isEmpty()) {
+				for (int i = 0; i < multipartFile.size(); i++) {
+
+					String originalName = multipartFile.get(i).getOriginalFilename();
 					String realPath = servletContext.getRealPath("/upload/board");
-					DateFormat df = new SimpleDateFormat("yyMMdd");//MM대문자가 월 /소문자 분(minute)
+					DateFormat df = new SimpleDateFormat("yyMMdd");// MM대문자가 월 /소문자 분(minute)
 					String saveFolder = df.format(new Date());
-					String realSaveFolder = realPath + File.separator + saveFolder; 
+					String realSaveFolder = realPath + File.separator + saveFolder;
 					File dir = new File(realSaveFolder);
-					
-					System.out.println("controller에서 저장된 경로!! : "+realPath);
-					if(!dir.exists())
-						dir.mkdirs();//폴더까지 생성
-					
-					String savedName = UUID.randomUUID().toString() + originalName.substring(originalName.lastIndexOf('.'));
-					
+
+					System.out.println("controller에서 저장된 경로!! : " + realPath);
+					if (!dir.exists())
+						dir.mkdirs();// 폴더까지 생성
+
+					String savedName = UUID.randomUUID().toString()
+							+ originalName.substring(originalName.lastIndexOf('.'));
+
 					File file = new File(realSaveFolder, savedName);
-					
+
 					try {
-						multipartFile.transferTo(file);
+						multipartFile.get(i).transferTo(file);
 					} catch (IllegalStateException e) {
 						e.printStackTrace();
 					} catch (IOException e) {
 						e.printStackTrace();
 					}
-					
+
 					bbsDto.setOriginalName(originalName);
 					bbsDto.setSavedName(savedName);
 					bbsDto.setSaveFolder(saveFolder);
-				} 
-			
-				boardNo = boardService.writeArticle(bbsDto);
-			
-			
-			
-			
+				}
+			}
+
+			boardNo = boardService.writeArticle(bbsDto);
+
 			if (boardNo != 0) {
 				model.addAttribute("boardNo", boardNo);
 				path = "board/writeok";
 			} else {
 				path = "오류 페이지 넣어야됨";
 			}
-		
+
 		} else {
 			path = "redirect:/index.jsp";
 		}
 		model.addAttribute("parameter", parameter);
-		//System.out.println("write controller out : 	" +parameter);
+		// System.out.println("write controller out : " +parameter);
 		return path;
 	}
-	
+
 //------------------------[게시물 보여주기]------------------------------
 	@RequestMapping(value = "/view", method = RequestMethod.GET)
 	public String view(@RequestParam("boardNo") int boardNo, @RequestParam Map<String, String> parameter, Model model,
@@ -150,11 +149,10 @@ public class BoardController {
 			parameter.put("boardNo", Integer.toString(boardNo));
 			int isPrev = boardService.checkPrev(parameter);
 			int isNext = boardService.checkNext(parameter);
-			
+
 			int totalLike = boardService.totalLike(boardNo);
 			int totalHate = boardService.totalHate(boardNo);
-			
-			
+
 			model.addAttribute("totalLike", totalLike);
 			model.addAttribute("totalHate", totalHate);
 			model.addAttribute("article", bbsDto);
@@ -169,168 +167,160 @@ public class BoardController {
 		return path;
 	}
 
-
 //------------------------[게시판 리스트]------------------------------------
 	@RequestMapping(value = "/list", method = RequestMethod.GET)
 	public String list(@RequestParam Map<String, String> parameter, Model model, HttpServletRequest requset) {
-		
-		//System.out.println(parameter);
+
+		// System.out.println(parameter);
 		List<BbsDto> list = boardService.listArticle(parameter);
-		//System.out.println("list C : "  + list);
-	
+		// System.out.println("list C : " + list);
+
 		PageNavigation pageNavigation = boardCommonService.getPageNavigation(parameter);
 		pageNavigation.setRoot(requset.getContextPath());
 		pageNavigation.makeNavigator();
-		//System.out.println("controller : " + parameter);
+		// System.out.println("controller : " + parameter);
 		model.addAttribute("parameter", parameter);
 		model.addAttribute("articleList", list);
 		model.addAttribute("navigator", pageNavigation);
-		
+
 		return "board/list";
 	}
 
-	
 //-----------------------------------[수정기능]--------------------------------------------------
-		
-		//수정페이지로 이동
-		@RequestMapping(value = "/modify", method = RequestMethod.GET)
-		public String modify(@RequestParam("boardNo") int boardNo, 
-								@RequestParam Map<String, String> parameter, Model model) {
-				String path = "";
-			
-				BbsDto bbsDto = boardService.getArticle(boardNo);
-				bbsDto.setBoardCategory(Integer.parseInt(parameter.get("boardCategory")));
-				model.addAttribute("article", bbsDto);
-				model.addAttribute("parameter", parameter);
-				path = "board/modify";
-			return path;
-		}
 
-		
-		
+	// 수정페이지로 이동
+	@RequestMapping(value = "/modify", method = RequestMethod.GET)
+	public String modify(@RequestParam("boardNo") int boardNo, @RequestParam Map<String, String> parameter,
+			Model model) {
+		String path = "";
+
+		BbsDto bbsDto = boardService.getArticle(boardNo);
+		bbsDto.setBoardCategory(Integer.parseInt(parameter.get("boardCategory")));
+		model.addAttribute("article", bbsDto);
+		model.addAttribute("parameter", parameter);
+		path = "board/modify";
+		return path;
+	}
+
 //수정 완료 버튼 클릭
-		@RequestMapping(value = "/modify", method = RequestMethod.POST)
-		public String modify(BbsDto bbsDto, 
-				@RequestParam Map<String, String> parameter, Model model,
-				HttpSession session) {
-			String path = "";
-			MemberDto memberDto = (MemberDto) session.getAttribute("userInfo");
+	@RequestMapping(value = "/modify", method = RequestMethod.POST)
+	public String modify(BbsDto bbsDto, @RequestParam Map<String, String> parameter, Model model, HttpSession session) {
+		String path = "";
+		MemberDto memberDto = (MemberDto) session.getAttribute("userInfo");
 
-			if (memberDto != null) {
+		if (memberDto != null) {
 
-				int boardNo = boardService.modifyArticle(bbsDto);
-				if (boardNo != 0) {
-					model.addAttribute("boardNo", boardNo);
-					path = "board/writeok";
-				} else {
-					//수정 실패했습니다 만들어야
-					path = "redirect:/index.jsp";
-				}
+			int boardNo = boardService.modifyArticle(bbsDto);
+			if (boardNo != 0) {
+				model.addAttribute("boardNo", boardNo);
+				path = "board/writeok";
 			} else {
-
-				path = "";
+				// 수정 실패했습니다 만들어야
+				path = "redirect:/index.jsp";
 			}
-			model.addAttribute("parameter", parameter);
-			return path;
+		} else {
+
+			path = "";
 		}
+		model.addAttribute("parameter", parameter);
+		return path;
+	}
 
 //-------------------------------[이전 글 보기]-----------------------------
-		@RequestMapping(value = "/prev", method = RequestMethod.GET)
-		public String prev(@RequestParam("boardNo") int boardNo, @RequestParam Map<String, String> parameter, Model model,
-				HttpSession session) {
-			String path = "";
-			MemberDto memberDto = (MemberDto) session.getAttribute("userInfo");
-			if (memberDto != null) {
-				
-				BbsDto bbsDto = boardService.prevArticle(parameter);
-				int isPrev = boardService.checkPrev(parameter);
-				model.addAttribute("isPrev", isPrev);
-				model.addAttribute("article", bbsDto);
-				model.addAttribute("parameter", parameter);
-				model.addAttribute("boardNo", parameter.get("boardNo"));
-				
-				path = "board/view";
-			} else {
-				path = "redirect:/index.jsp";
-			}
-			return path;
+	@RequestMapping(value = "/prev", method = RequestMethod.GET)
+	public String prev(@RequestParam("boardNo") int boardNo, @RequestParam Map<String, String> parameter, Model model,
+			HttpSession session) {
+		String path = "";
+		MemberDto memberDto = (MemberDto) session.getAttribute("userInfo");
+		if (memberDto != null) {
+
+			BbsDto bbsDto = boardService.prevArticle(parameter);
+			int isPrev = boardService.checkPrev(parameter);
+			model.addAttribute("isPrev", isPrev);
+			model.addAttribute("article", bbsDto);
+			model.addAttribute("parameter", parameter);
+			model.addAttribute("boardNo", parameter.get("boardNo"));
+
+			path = "board/view";
+		} else {
+			path = "redirect:/index.jsp";
 		}
+		return path;
+	}
 
 //-------------------------------[다음 글 보기]-----------------------------
-		@RequestMapping(value = "/next", method = RequestMethod.GET)
-		public String next(@RequestParam("boardNo") int boardNo, @RequestParam Map<String, String> parameter, Model model,
-				HttpSession session) {
-			String path = "";
-			MemberDto memberDto = (MemberDto) session.getAttribute("userInfo");
-			if (memberDto != null) {
-				BbsDto bbsDto = boardService.nextArticle(parameter);
-				int isNext = boardService.checkNext(parameter);
-				model.addAttribute("article", bbsDto);
-				model.addAttribute("parameter", parameter);
-				model.addAttribute("boardNo", parameter.get("boardNo"));
-				model.addAttribute("isNext", isNext);
-				
-				path = "board/view";
-			} else {
-				path = "redirect:/index.jsp";
-			}
-			return path;
+	@RequestMapping(value = "/next", method = RequestMethod.GET)
+	public String next(@RequestParam("boardNo") int boardNo, @RequestParam Map<String, String> parameter, Model model,
+			HttpSession session) {
+		String path = "";
+		MemberDto memberDto = (MemberDto) session.getAttribute("userInfo");
+		if (memberDto != null) {
+			BbsDto bbsDto = boardService.nextArticle(parameter);
+			int isNext = boardService.checkNext(parameter);
+			model.addAttribute("article", bbsDto);
+			model.addAttribute("parameter", parameter);
+			model.addAttribute("boardNo", parameter.get("boardNo"));
+			model.addAttribute("isNext", isNext);
+
+			path = "board/view";
+		} else {
+			path = "redirect:/index.jsp";
 		}
-		
+		return path;
+	}
 
 //-------------------------------[글 삭제]------------------------------
-		@RequestMapping(value = "/delete", method = RequestMethod.GET)
-		public String delete(@RequestParam("boardNo") int boardNo, @RequestParam Map<String, String> parameter, Model model,
-				HttpSession session) {
-			MemberDto memberDto = (MemberDto) session.getAttribute("userInfo");
-			String path = "";
-			//System.out.println("C :" +parameter);
-			if (memberDto != null) {
-				
-				boardService.deleteArticle(boardNo);
-				if (boardNo != 0) {
-					model.addAttribute("boardNo", boardNo);
-					model.addAttribute("parameter", parameter);
-					path = "board/deleteok";
-				} else {
-					path = "오류 페이지 넣어야됨";
-				}
-			
+	@RequestMapping(value = "/delete", method = RequestMethod.GET)
+	public String delete(@RequestParam("boardNo") int boardNo, @RequestParam Map<String, String> parameter, Model model,
+			HttpSession session) {
+		MemberDto memberDto = (MemberDto) session.getAttribute("userInfo");
+		String path = "";
+		// System.out.println("C :" +parameter);
+		if (memberDto != null) {
+
+			boardService.deleteArticle(boardNo);
+			if (boardNo != 0) {
+				model.addAttribute("boardNo", boardNo);
+				model.addAttribute("parameter", parameter);
+				path = "board/deleteok";
 			} else {
-				path = "redirect:/index.jsp";
+				path = "오류 페이지 넣어야됨";
 			}
-			return path;
+
+		} else {
+			path = "redirect:/index.jsp";
 		}
-		
-	
-	//----------------------------------[좋아요, 싫어요]---------------------------------
-		
-		@RequestMapping(value = "/like", method = RequestMethod.POST, consumes = "application/json", headers = {
-		"Content-type=application/json" })
-		public @ResponseBody String like(@RequestBody LoveDto loveDto, Model model, HttpSession session) {
-			String likeCheck = "";
-			MemberDto memberDto = (MemberDto) session.getAttribute("userInfo");
-			String json = "";
-			if (memberDto != null) {
-				
-				loveDto.setUserId(memberDto.getUserId());
-				
-				System.out.println("client -> controller : " + loveDto);
-				int boardNo = loveDto.getBoardNo();
-				
-				likeCheck = boardService.likeCount(loveDto);
-				int totalLike = boardService.totalLike(boardNo);
-				int totalHate = boardService.totalHate(boardNo);
-				System.out.println("controller -> client : " + likeCheck);
-				
-				json = "{\"likeCheck\" : \""+likeCheck+"\",\"totalLike\" : \""+totalLike+"\",\"totalHate\" : \""+totalHate+"\"}";
-				return json;
-			} else {
-				likeCheck = "";
-			}
-			
+		return path;
+	}
+
+	// ----------------------------------[좋아요, 싫어요]---------------------------------
+
+	@RequestMapping(value = "/like", method = RequestMethod.POST, consumes = "application/json", headers = {
+			"Content-type=application/json" })
+	public @ResponseBody String like(@RequestBody LoveDto loveDto, Model model, HttpSession session) {
+		String likeCheck = "";
+		MemberDto memberDto = (MemberDto) session.getAttribute("userInfo");
+		String json = "";
+		if (memberDto != null) {
+
+			loveDto.setUserId(memberDto.getUserId());
+
+			System.out.println("client -> controller : " + loveDto);
+			int boardNo = loveDto.getBoardNo();
+
+			likeCheck = boardService.likeCount(loveDto);
+			int totalLike = boardService.totalLike(boardNo);
+			int totalHate = boardService.totalHate(boardNo);
+			System.out.println("controller -> client : " + likeCheck);
+
+			json = "{\"likeCheck\" : \"" + likeCheck + "\",\"totalLike\" : \"" + totalLike + "\",\"totalHate\" : \""
+					+ totalHate + "\"}";
 			return json;
+		} else {
+			likeCheck = "";
 		}
-		
-	
+
+		return json;
+	}
+
 }
